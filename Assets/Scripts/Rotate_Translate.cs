3b3c9383630e48ebf922c043063149749aa1b6e6
Jacob Willemsz <willemszjacob@gmail.com>
2023-04-30 15:29:02 -0700

    Adjust imposter spinraate

--- Assets/Scripts/Rotate_Translate.cs
+++ Assets/Scripts/Rotate_Translate.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class Rotate_Translate : MonoBehaviour
 {
     [SerializeField] private Vector3 _rotation;
+    [SerializeField] private float _speed;
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(_rotation * Time.deltaTime);
+        transform.Rotate(_rotation * _speed * Time.deltaTime);
 
         transform.position += new Vector3(1, 0, 0) * Time.deltaTime;
     }
